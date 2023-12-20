3dfb7f5b87e7901fbfdcb5db4572caa9d260cd5f
Jens Wiklander <jens.wiklander@linaro.org>
2023-12-20 11:17:56 +0100

    xen/arm: ffa: support notification
    Add support for FF-A notifications, currently limited to an SP (Secure
    Partition) sending an asynchronous notification to a guest.
    
    Guests and Xen itself are made aware of pending notifications with an
    interrupt. The interrupt handler retrieves the notifications using the
    FF-A ABI and deliver them to their destinations.
    
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- xen/arch/arm/tee/ffa.c
+++ xen/arch/arm/tee/ffa.c
@@ -194,6 +194,8 @@ out:
 
 static void handle_features(struct cpu_user_regs *regs)
 {
+    struct domain *d = current->domain;
+    struct ffa_ctx *ctx = d->arch.tee;
     uint32_t a1 = get_user_reg(regs, 1);
     unsigned int n;
 
@@ -239,6 +241,30 @@ static void handle_features(struct cpu_user_regs *regs)
          */
         BUILD_BUG_ON(PAGE_SIZE != FFA_PAGE_SIZE);
         ffa_set_regs_success(regs, 0, 0);
+        break;
+    case FFA_FEATURE_NOTIF_PEND_INTR:
+        if ( ctx->notif.enabled )
+            ffa_set_regs_success(regs, ctx->notif.np_intid, 0);
+        else
+            ffa_set_regs_error(regs, FFA_RET_NOT_SUPPORTED);
+        break;
+    case FFA_FEATURE_SCHEDULE_RECV_INTR:
+        if ( ctx->notif.enabled )
+            ffa_set_regs_success(regs, ctx->notif.sr_intid, 0);
+        else
+            ffa_set_regs_error(regs, FFA_RET_NOT_SUPPORTED);
+        break;
+
+    case FFA_NOTIFICATION_BIND:
+    case FFA_NOTIFICATION_UNBIND:
+    case FFA_NOTIFICATION_GET:
+    case FFA_NOTIFICATION_SET:
+    case FFA_NOTIFICATION_INFO_GET_32:
+    case FFA_NOTIFICATION_INFO_GET_64:
+        if ( ctx->notif.enabled )
+            ffa_set_regs_success(regs, 0, 0);
+        else
+            ffa_set_regs_error(regs, FFA_RET_NOT_SUPPORTED);
         break;
     default:
         ffa_set_regs_error(regs, FFA_RET_NOT_SUPPORTED);
@@ -321,6 +347,22 @@ static bool ffa_handle_call(struct cpu_user_regs *regs)
         else
             ffa_set_regs_success(regs, 0, 0);
         return true;
+    case FFA_NOTIFICATION_BIND:
+        ffa_handle_notification_bind(regs);
+        return true;
+    case FFA_NOTIFICATION_UNBIND:
+        ffa_handle_notification_unbind(regs);
+        return true;
+    case FFA_NOTIFICATION_INFO_GET_32:
+    case FFA_NOTIFICATION_INFO_GET_64:
+        ffa_handle_notification_info_get(regs);
+        return true;
+    case FFA_NOTIFICATION_GET:
+        ffa_handle_notification_get(regs);
+        return true;
+    case FFA_NOTIFICATION_SET:
+        ffa_handle_notification_set(regs);
+        return true;
 
     default:
         gprintk(XENLOG_ERR, "ffa: unhandled fid 0x%x\n", fid);
@@ -357,6 +399,9 @@ static int ffa_domain_init(struct domain *d)
 
     if ( !ffa_partinfo_domain_init(d) )
         return -EIO;
+
+    if ( !ffa_notif_domain_init(d) )
+        return -ENOMEM;
 
     return 0;
 }
@@ -433,6 +478,7 @@ static int ffa_domain_teardown(struct domain *d)
         return 0;
 
     ffa_rxtx_domain_destroy(d);
+    ffa_notif_domain_destroy(d);
 
     ffa_domain_teardown_continue(ctx, true /* first_time */);
 
@@ -512,6 +558,7 @@ static bool ffa_probe(void)
     if ( !ffa_partinfo_init() )
         goto err_rxtx_destroy;
 
+    ffa_notif_init();
     INIT_LIST_HEAD(&ffa_teardown_head);
     init_timer(&ffa_teardown_timer, ffa_teardown_timer_callback, NULL, 0);
 
